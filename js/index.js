14d120d1db9cbfeb3675363693993d810f03cb9e
Anu Khambete <anukhambete@gmail.com>
2018-11-28 02:19:15 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var element = document.createElement('div');
+var ul = document.createElement('ul');
+for(let i=0;i<4;i++) {
+let li = document.createElement('li');
+li.innerHTML = (i*i).toString();
+ul.appendChild(li);
+}
+element.appendChild('ul');
